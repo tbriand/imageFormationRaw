4fcad308bcc604223439ea161144aa9e88c61746
Thibaud Briand (Dell notebook) <briand.thibaud@gmail.com>
2019-08-13 08:58:58 -0400

    Add sigma_nc as parameter in fusion script

--- scripts/fusion_irregularly_sampled_data.sh
+++ scripts/fusion_irregularly_sampled_data.sh
@@ -3,8 +3,8 @@
 
 # read input parameters
 if [ "$#" -lt "5" ]; then
-    echo "usage: $0 inpath homo_path output_image number raw zoom"
-    echo "example: $0 im_%i.tiff homo_%i.tiff out.tiff 101 1 2"
+    echo "usage: $0 inpath homo_path output_image number raw zoom sigma_nc"
+    echo "example: $0 im_%i.tiff homo_%i.tiff out.tiff 101 1 2 0.7"
     exit 1
 fi
 
@@ -17,13 +17,16 @@ ZOOM=$6
 if [ -z "$ZOOM" ]; then
     ZOOM=1
 fi
+SIGMANC=$7
+if [ -z "$SIGMANC" ]; then
+    SIGMANC=0.70710678118 # scale of the gaussian weight (sqrt(2)/2)
+fi 
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 PATH=${SCRIPTPATH%/*}/build/:$PATH
 
 # set parameters
 ind_ini=2 # change to 1 to use the reference image during the fusion
-sigma=0.70710678118 # scale of the gaussian weight (sqrt(2)/2)
 
 # use order 2 except without zoom for standard images
 order=2 
@@ -33,13 +36,13 @@ fi
 
 # irregularly sampled data fitting
 echo "Starting data accumulation and blurry image computation"
-    combi_ckr $INPATH $HOMOPATH $ind_ini $NUMBER $ZOOM $OUT $order $sigma $RAW
+    combi_ckr $INPATH $HOMOPATH $ind_ini $NUMBER $ZOOM $OUT $order $SIGMANC $RAW
 
 # sharpening step
 echo "Sharpening step"
 
     cropv=10 # the image is cropped before and after the sharpening step to remove the boundary artefacts
     crop $cropv $cropv -$cropv -$cropv $OUT $OUT
-    asymptotic_nc_filter $OUT $OUT $order $sigma 1
+    asymptotic_nc_filter $OUT $OUT $order $SIGMANC 1
     crop $cropv $cropv -$cropv -$cropv $OUT $OUT
 
